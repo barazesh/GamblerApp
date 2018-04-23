d6391f4009f3be594bf3e36d99e2fefa98efb19a
Mohammadreza Barazesh <barazesh.mr90@gmail.com>
2018-04-23 21:39:02 +0430

    creating the project

---
+++ GamblerFormsApp/GamblerFormsApp/State.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamblerFormsApp
+{
+    class State
+    {
+        private int _value;
+
+        public int Value { get => _value; set => _value = value; }
+    }
+}
